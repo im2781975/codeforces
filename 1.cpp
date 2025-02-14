2e8b281dfc7c636501e922d759311a39aa9a8f48
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-14 11:50:08 +0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,5 @@
 using namespace std;
-//problem/369/A_given n dishes, where each dish requires either a bowl or a plate 
+//problem/369/A _given n dishes, where each dish requires either a bowl or a plate 
 //have a limited number of bowls and plates.determine how many dishes cannot be served due to a lack of resources.
 int main(){
     int n, bowl, plate; cin >> n >> bowl >> plate;
@@ -32,7 +32,39 @@ int main(){
     cout << ans + res;
 }
 using namespace std;
-//contest/1680/problem/A_calculate the minimum possible number of elements in a array by which range [l1, r1] elements 
+// contest/1579/problem/E1 _Given a permutation of size n,need to construct a deque by sequentially adding elements. starting smallest value.
+//Before adding each value choose whether to add it to the front or the back of the deque.determine the final order of elements in the deque
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        list <int> lst;
+        vector <int> vec(n);
+        int i = 0;
+        for(int i = 0; i < n; i++)
+            cin >> vec[i];
+        while(i < n){
+            if(i == 0){
+                lst.push_back(vec[i]);
+                i++;
+            }
+            else{
+                if (vec[i] < lst.front())
+                    lst.push_front(vec[i]);
+                else
+                    lst.push_back(vec[i]);
+                i++;
+            }
+        }
+        while (!lst.empty()){
+            cout << lst.front() << " ";
+            lst.pop_front();
+        }
+        cout << "\n";
+    }
+}
+using namespace std;
+//contest/1680/problem/A _calculate the minimum possible number of elements in a array by which range [l1, r1] elements 
 // equal to its minimum and [l2, r2] elements in the array equal to its maximum.
 int main(){
     int l1, l2, r1, r2, res;
@@ -51,7 +83,7 @@ int main(){
     }
 }
 using namespace std;
-//problem/1760/A_find the medium number among three integers
+//problem/1760/A _find the medium number among three integers
 int medium(int a, int b, int c){
     int maxi = a, mini = a;
     if(b > maxi) maxi = b;
@@ -68,7 +100,7 @@ int main(){
     }
 }
 using namespace std;
-//problem/1857/A_determine whether it is possible to color all its elements in two colors in such a way that the sums 
+//problem/1857/A _determine whether it is possible to color all its elements in two colors in such a way that the sums 
 //of the elements of both colors have the same parity and each color has at least one element colored.
 int main(){
     int t; cin >> t;
